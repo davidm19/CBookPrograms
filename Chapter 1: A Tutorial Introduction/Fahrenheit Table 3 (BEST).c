baf0f2ba5bab82a67b4a7908e2e681d00fb6cba1
Broccoli Boy <dmalone@chadwickschool.org>
2018-03-19 17:11:09 -0700

    Continue from External Variables and Scope

--- Chapter 1: A Tutorial Introduction/Fahrenheit Table 3 (BEST).c
+++ Chapter 1: A Tutorial Introduction/Fahrenheit Table 3 (BEST).c
@@ -10,7 +10,7 @@ int main()
 	int fahr;
 
 	for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
-		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
 	}
 
 	return 0;
